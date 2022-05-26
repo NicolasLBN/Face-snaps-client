80fa5035598034c16eac090cb563321c93571404
nicolas <niko83136@gmail.com>
2022-05-26 19:45:11 +0200

    add face snap component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaceSnapComponent
   ],
   imports: [
     BrowserModule,
